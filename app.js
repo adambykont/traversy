348060082c33b7973b90b0628c2ebb179e3274a2
janusz <janusz.agh@gmail.com>
2020-05-06 21:50:58 +0200

    delete items eventListener

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-let element = document.querySelector('.clear-tasks');
-element.addEventListener('click', (e) => {
-    // e.preventDefault();
-    console.log('clicked');
-});
+let items = document.querySelector('.collection');
+
+function deleteItem(e) {
+    if (e.target.classList.contains('fa-remove')) {
+        let liEl = e.target.parentElement.parentElement;
+        console.log(liEl);
+        liEl.parentElement.removeChild(liEl);
+    }
+}
+
+items.addEventListener('click', deleteItem);
