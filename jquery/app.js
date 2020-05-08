6f521aa1bda652bf210a9852cafef6fa908fa988
janusz <janusz.agh@gmail.com>
2020-05-08 17:32:50 +0200

    3 buttons

--- jquery/app.js
+++ jquery/app.js
@@ -1,3 +1,9 @@
-function hide(e, speed) {
-    $(e).hide(speed);
-}
+$('#btn1').click(
+    () => $('#btn-feedback').html('First button has been clicked')
+)
+$('#btn2').click(
+    () => $('#btn-feedback').html('Second button has been clicked')
+)
+$('#btn3').click(
+    () => $('#btn-feedback').html('Third button has been clicked')
+)
