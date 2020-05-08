2e9b5209b044f07090a296d6c03f6bb2f3a786b9
janusz <janusz.agh@gmail.com>
2020-05-08 19:21:57 +0200

    toggle button

--- jquery/app.js
+++ jquery/app.js
@@ -1,9 +1,3 @@
-$('#btn1').click(
-    () => $('#btn-feedback').html('First button has been clicked')
-)
-$('#btn2').click(
-    () => $('#btn-feedback').html('Second button has been clicked')
-)
-$('#btn3').click(
-    () => $('#btn-feedback').html('Third button has been clicked')
-)
+function toggle(element, speed) {
+$(element).toggle(speed);
+}
