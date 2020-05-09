6325e731166f07cf6b3c9585341762d20f23c8e5
janusz <janusz.agh@gmail.com>
2020-05-09 21:55:19 +0200

    ajax json

--- oojs/app.js
+++ oojs/app.js
@@ -1,20 +1,20 @@
 function loadData(event) {
 
+
     let xhr = new XMLHttpRequest();
 
-    console.log(xhr.readyState);
 
     xhr.open('get', 'data.txt', true);
-    console.log(xhr.readyState);
 
-    xhr.onreadystatechange = () => {
-        console.log(xhr.readyState);
-        if (xhr.status === 200 && xhr.readyState === 4) {
-            document.getElementById('output').innerHTML = xhr.responseText;
+    xhr.onload = function () {
+        console.log(this);
+        if (this.status === 200 && this.readyState === 4) {
+            document.getElementById('output').innerHTML = this.responseText;
         }
-    }
+    };
 
     xhr.send();
+    // console.log(xhr);
 
     event.preventDefault();
 }
