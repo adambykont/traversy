63bd92295e563e85ca4d269c5b065653159f94a9
janusz <janusz.agh@gmail.com>
2020-05-10 19:14:49 +0200

    api skeleton

---
+++ oojs/apis/easyhttp.js
@@ -0,0 +1,11 @@
+function easyHTTP() {
+    this.http = new XMLHttpRequest();
+}
+
+// Make a HTTP GET Request
+
+// Make a HTTP POST Request
+
+// Make a HTTP UPDATE Request
+
+// Make a HTTP DELETE Request
